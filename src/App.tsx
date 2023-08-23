11d3feb2f87e96ab729bca7667d4701449abbdf5
sergiogenes <genessergio@gmail.com>
2023-08-23 16:15:36 -0300

    Se inicio aplicación, se configuró eslint y prettier

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+import { CardQuiz } from './componentes/CardQuiz'
+
+function App() {
+  return (
+    <div className='app-container'>
+      <CardQuiz />
+    </div>
+  )
+}
+
+export default App
---
+++ src/componentes/CardQuiz/index.tsx
@@ -0,0 +1,5 @@
+import './index.css'
+
+export const CardQuiz = () => {
+  return <h1>Card Quiz</h1>
+}
