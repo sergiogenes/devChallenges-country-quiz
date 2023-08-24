f96c8d849abf806756f759327a11084307fe39be
sergiogenes <genessergio@gmail.com>
2023-08-24 18:59:11 -0300

    Se realizaron algunas pruebas a la API y se empezó con el componente CardQuiz

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,46 @@
+import { useEffect, useState } from 'react'
 import './App.css'
 import { CardQuiz } from './componentes/CardQuiz'
 
+interface Country {
+  id: String
+  flags: Flag
+  capital: Array<String>
+  name: String
+}
+type CountryWithoutName = Omit<Country, 'name'>
+
+interface CountryFromAPI extends CountryWithoutName {
+  name: {
+    official: String
+  }
+}
+
+interface Flag {
+  png: String
+  svg: String
+  alt: String
+}
+
 function App() {
+  const [countries, SetCountries] = useState<Country[]>([])
+
+  useEffect(() => {
+    fetch('https://restcountries.com/v3.1/all?fields=name,capital,flags')
+      .then((res) => res.json())
+      .then((data: CountryFromAPI[]) => {
+        const allCountries = data.map((country) => {
+          const id = self.crypto.randomUUID()
+          return { ...country, id, name: country.name.official }
+        })
+        SetCountries(allCountries)
+      })
+  }, [])
+
+  console.log('Countries ==>>', countries)
+
+  let id = self.crypto.randomUUID()
+  console.log('id ==>>', id)
   return (
     <div className='app-container'>
       <CardQuiz />
--- src/componentes/CardQuiz/index.tsx
+++ src/componentes/CardQuiz/index.tsx
@@ -1,5 +1,20 @@
+import logo from '../../assets/undraw_adventure_4hum 1.svg'
 import './index.css'
 
 export const CardQuiz = () => {
-  return <h1>Card Quiz</h1>
+  return (
+    <div className='card-container'>
+      <header>
+        <h1>Country quiz</h1>
+        <img src={logo} alt='imagen decorativa de la card de proguntas' />
+      </header>
+      <p>Kuala Lumpur is the capital of</p>
+      <ol>
+        <li>Vietnam</li>
+        <li>Malaysia</li>
+        <li>Sweden</li>
+        <li>Austria</li>
+      </ol>
+    </div>
+  )
 }
