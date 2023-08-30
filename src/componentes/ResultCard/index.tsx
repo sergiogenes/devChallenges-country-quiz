ee695eb46f0ba07e676424bfe89ce88c2370e2ef
sergiogenes <genessergio@gmail.com>
2023-08-30 10:00:53 -0300

    Se agregó funcionalidad restar game en Result Card component

--- src/componentes/ResultCard/index.tsx
+++ src/componentes/ResultCard/index.tsx
@@ -2,11 +2,20 @@ import { useSelector } from 'react-redux'
 import './index.css'
 import winner from '../../assets/undraw_winners_ao2o 2.svg'
 import { RootState } from '../../state/store'
+import { useDispatch } from 'react-redux'
+import { setGameState } from '../../state/gameStateSlice'
+import { resetQuestionState } from '../../state/numberOfQuestionSlice'
 
 export const ResultCard = () => {
+  const dispatch = useDispatch()
   const correctAnswers = useSelector(
     (state: RootState) => state.numberOfQuestions.correctAnswers,
   )
+
+  const handleRestarGame = () => {
+    dispatch(setGameState('begin'))
+    dispatch(resetQuestionState())
+  }
 
   return (
     <div id='result-card'>
@@ -18,7 +27,7 @@ export const ResultCard = () => {
       <p>
         You got <span>{correctAnswers}</span> correct answers
       </p>
-      <button>Try again</button>
+      <button onClick={handleRestarGame}>Try again</button>
     </div>
   )
 }
